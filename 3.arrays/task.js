74134ebc50574b97ea2b2011a37bc06183ae4e9f
Aleksandra Kameneva <kameneva_fkp@mail.ru>
2021-09-16 21:53:28 +0500

    ДЗ 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,25 @@
 function compareArrays(arr1, arr2) {
   let result;
+  if (arr1.length===arr2.length) {
+  	result = arr1.every(function(current,i) {
+		return current===arr2[i];
+  	});
+  } else {
+  	result = false;
+  }
 
-  // Ваш код
-
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter((item) => {
+  	if (item > 0 && item % 3 === 0) return true;
+  }).map((element) => {
+  	return element *10
+  });
+    
 
   return resultArr; // array
 }
