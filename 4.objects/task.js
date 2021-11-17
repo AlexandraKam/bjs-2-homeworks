18b1886a26d501a99247a31389cdc5e5688005ec
Aleksandra Kameneva <kameneva_fkp@mail.ru>
2021-11-17 22:32:46 +0500

    ДЗ 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
 }
+
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+	
+  if(this.marks === undefined){ 
+    this.marks = [];
+    this.marks.push(mark); 
+    } else {
+      this.marks.push(mark);
+    }
+   
+}
+
+Student.prototype.addMarks = function (...marks) {
+	this.marks = marks;
+}
+
+Student.prototype.getAverage = function () {
+	let sum = 0;
+	for (let i = 0; i < this.marks.length; i++) {
+  	sum += this.marks[i];
+  }
+	
+	return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+	this.excluded = reason;
+	delete this.subject;
+	delete this.marks;
+}
