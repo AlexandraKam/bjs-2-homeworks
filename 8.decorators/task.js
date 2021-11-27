19b0c835596d9c54115f65f99da70dfc139838b3
Aleksandra Kameneva <kameneva_fkp@mail.ru>
2021-11-27 22:02:52 +0500

    ДЗ 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,52 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = args.join(',');
+		let idx = cache.findIndex((item)=> item.hash === hash);
+		if (idx !== -1 ) {
+			console.log("Из кэша: " + cache[idx].value);
+			return "Из кэша: " + cache[idx].value;
+        } else {
+        	let result = func(...args);
+        	cache.push({hash: hash, value: result});
+        	if (cache.length > 5) {
+        		cache.shift();
+        	}
+        	console.log("Вычисляем: " + result);
+        	return "Вычисляем: " + result; 
+        }
+	}
+	return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, interval) {
+	let isCalled;
+	return function () {
+		if (!isCalled) {
+			func();
+			isCalled = true;
+			setTimeout(function() {
+				isCalled = false;
+			}, interval);
+		}
+	}
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, interval) {
+	let isCalled;
+	function wrapper() {
+		if (!isCalled) {
+			func();
+			isCalled = true;
+			setTimeout(function() {
+				isCalled = false;
+			}, interval);
+		}
+		wrapper.count ++;
+	}
+	wrapper.count = 0;	 
+	return wrapper;
 }
+
